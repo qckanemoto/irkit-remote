7d9a9966de806bbfcd64785461107fe8a0024b9a
Takashi Kanemoto <kanemoto.takashi@gmail.com>
2015-11-18 01:11:12 +0900

    install material-ui

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var $ = require('jquery');
+var injectTapEventPlugin = require('react-tap-event-plugin');
+
+const RaisedButton = require('material-ui/lib/raised-button');
+
+// needed for onTouchTap
+injectTapEventPlugin();
 
 ReactDOM.render(
-    <h1>hello</h1>,
-    document.getElementById('content')
+    <RaisedButton label="Super Secret Password" primary={true} />,
+    document.getElementById('app')
 );
