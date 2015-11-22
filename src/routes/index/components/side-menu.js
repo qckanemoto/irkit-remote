3d085c6b5276768e1a771036fe1b6b41670dce42
Takashi Kanemoto <kanemoto.takashi@gmail.com>
2015-11-22 19:31:33 +0900

    add ui

--- src/routes/index/components/side-menu.js
+++ src/routes/index/components/side-menu.js
@@ -3,6 +3,7 @@ const Panel = require('react-bootstrap').Panel;
 const ListGroup = require('react-bootstrap').ListGroup;
 const ListGroupItem = require('react-bootstrap').ListGroupItem;
 const LinkContainer = require('react-router-bootstrap').LinkContainer;
+const Button = require('react-bootstrap').Button;
 const _ = require('lodash');
 
 // to be removed
@@ -19,11 +20,13 @@ module.exports = React.createClass({
             );
         });
         return (
-            <Panel header={<h4><i className="fa fa-wifi fa-rotate-90"></i> IRKits</h4>}>
-                <ListGroup fill>
-                    {listGroupItems}
-                </ListGroup>
-            </Panel>
+            <div>
+                <Panel header={<h4><i className="fa fa-wifi fa-rotate-90"></i> IRKits</h4>}>
+                    <ListGroup fill>
+                        {listGroupItems}
+                    </ListGroup>
+                </Panel>
+            </div>
         );
     }
 });
--- src/routes/index/index.js
+++ src/routes/index/index.js
@@ -5,20 +5,34 @@ const Col = require('react-bootstrap').Col;
 const Button = require('react-bootstrap').Button;
 const Tabs = require('react-bootstrap').Tabs;
 const Tab = require('react-bootstrap').Tab;
+const Tooltip = require('react-bootstrap').Tooltip;
+const OverlayTrigger = require('react-bootstrap').OverlayTrigger;
 const _ = require('lodash');
 
-const DeviceSelector = require('./components/device-selector.js');
+const SideMenu = require('./components/side-menu.js');
 
 // to be removed
 const mockData = require('../../../tests/mock-data.js');
 
 module.exports = React.createClass({
+    getInitialState: function () {
+        return {isEditing: false};
+    },
+    componentDidMount: function () {
+    },
+    enterEditing: function () {
+        this.setState({isEditing: true});
+    },
+    cancelEditing: function () {
+        this.setState({isEditing: false});
+    },
     render: function () {
         var tabs = [];
+        var that = this;
         _.forEach(mockData.remote.tabs, function (t, i) {
             var buttons = [];
             _.forEach(t.buttons, function (b, j) {
-                buttons.push(<RemoteButton key={j} data={b} />)
+                buttons.push(<RemoteButton key={j} data={b} isEditing={that.state.isEditing} />)
             });
             tabs.push(
                 <Tab key={i} eventKey={i+1} title={t.name} style={{marginTop: '1em'}}>
@@ -33,12 +47,21 @@ module.exports = React.createClass({
             <Grid>
                 <Row>
                     <Col xs={12} md={6} mdPush={4}>
+                        <div className={this.state.isEditing && 'hidden'}>
+                            <OverlayTrigger overlay={<Tooltip id="edit-this-remote">Edit this remote</Tooltip>} placement="top" onClick={this.enterEditing}>
+                                <Button bsStyle="link" className="pull-right"><i className="fa fa-cog"></i></Button>
+                            </OverlayTrigger>
+                        </div>
                         <Tabs defaultActiveKey={1}>
                             {tabs}
                         </Tabs>
+                        <Row className={this.state.isEditing || 'hidden'} style={{marginBottom: '1em'}}>
+                            <Col xs={6}><Button bsStyle="default" block onClick={this.cancelEditing}><i className="fa fa-times"></i> Cancel</Button></Col>
+                            <Col xs={6}><Button bsStyle="primary" block><i className="fa fa-check"></i> Submit</Button></Col>
+                        </Row>
                     </Col>
                     <Col xs={12} md={2} mdOffset={2} mdPull={6}>
-                        <DeviceSelector />
+                        <SideMenu />
                     </Col>
                 </Row>
             </Grid>
@@ -48,17 +71,31 @@ module.exports = React.createClass({
 
 const RemoteButton = React.createClass({
     render: function () {
-        var buttonStyle = {color: '#666', height: '5.5em', padding: '.1em', marginBottom: '1em'};
+        var buttonStyle = {height: '5.5em', padding: '.1em', marginBottom: '1em'};
+        var labelStyle = {fontSize: '.8em', marginTop: '.5em'};
+
         if (_.isEmpty(this.props.data)) {
-            buttonStyle.visibility = 'hidden';
+            buttonStyle.borderStyle = 'dashed';
+            if (!this.props.isEditing) {
+                buttonStyle.visibility = 'hidden';
+            }
+            return (
+                <Col xs={3} style={{paddingLeft: '0', opacity: '.3'}}>
+                    <Button bsStyle="default" block style={buttonStyle}>
+                        <i className="fa fa-2x fa-plus"></i>
+                        <div style={labelStyle}>Empty</div>
+                    </Button>
+                </Col>
+            );
+        } else {
+            return (
+                <Col xs={3} style={{paddingLeft: '0'}}>
+                    <Button bsStyle="default" block style={buttonStyle}>
+                        <i className={'fa fa-2x fa-' + this.props.data.icon}></i>
+                        <div style={labelStyle}>{this.props.data.label}</div>
+                    </Button>
+                </Col>
+            );
         }
-        return (
-            <Col xs={3} style={{paddingLeft: '0'}}>
-                <Button bsStyle="default" block style={buttonStyle}>
-                    <i className={'fa fa-2x fa-' + this.props.data.icon}></i>
-                    <div style={{fontSize: '.8em', marginTop: '.5em'}}>{this.props.data.label}</div>
-                </Button>
-            </Col>
-        );
     }
 });
