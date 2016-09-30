8b81f8ea13a881ca84ad6cbba5b6c158e58b6d2b
Takashi Kanemoto <kanemoto.takashi@gmail.com>
2016-10-01 02:44:28 +0900

    install ng2-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { Ng2BootstrapModule } from 'ng2-bootstrap/ng2-bootstrap';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,7 +14,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    Ng2BootstrapModule
   ],
   providers: [],
   bootstrap: [AppComponent]
