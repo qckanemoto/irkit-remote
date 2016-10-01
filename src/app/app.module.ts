1fc998398663baec207a1ace923aa91ea9a55577
Takashi Kanemoto <kanemoto.takashi@gmail.com>
2016-10-01 09:15:37 +0900

    fix indents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,16 +8,16 @@ import { Ng2BootstrapModule } from 'ng2-bootstrap/ng2-bootstrap';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    Ng2BootstrapModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        Ng2BootstrapModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
