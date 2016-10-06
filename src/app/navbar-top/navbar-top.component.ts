d257430cc7efed57c878b8e9e23e22a6d8ab9bc0
Takashi Kanemoto <kanemoto.takashi@gmail.com>
2016-10-06 15:48:44 +0900

    add icon to navbar-top

--- src/app/navbar-top/navbar-top.component.ts
+++ src/app/navbar-top/navbar-top.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class NavbarTopComponent implements OnInit {
 
     @Input() heading: string;
+    @Input() icon: string;
 
     constructor() {
     }
